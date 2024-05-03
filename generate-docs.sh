2e8a126342e51bb89ba4442adad9d53e200d6548
Gui Valente <gui@vertical-life.info>
2024-05-03 12:28:04 +0200

    Fixed path of svg on md

--- generate-docs.sh
+++ generate-docs.sh
@@ -2,4 +2,4 @@ mmdc -i templates/learning-path.md -o ./learning-path.png -t neutral
 mmdc -i templates/learning-path.md -o ./learning-path.md -t neutral
 mv learning-path-*.svg images/
 mv learning-path-*.png images/
-# verybatim "![diagram](./" "![diagram](./images/" learning-path.md
+verybatim "![diagram](./" "![diagram](./images/" learning-path.md
