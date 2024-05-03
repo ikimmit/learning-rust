9175b081ba67d79c969018b94002513c76fdfb6e
Gui Valente <gui@vertical-life.info>
2024-05-03 12:49:39 +0200

    Resurrected svg map, file works locally

--- generate-docs.sh
+++ generate-docs.sh
@@ -1,8 +1,2 @@
-mmdc -i templates/learning-path.md -o ./learning-path.png -t neutral -b transparent -w 1000
-mv learning-path-*.png images/
-
-# Would be nice to deploy images and fonts inside svg inside markdown on github...
-#
-# mmdc -i templates/learning-path.md -o ./learning-path.md -t neutral
-# mv learning-path-*.svg images/
-# verybatim "![diagram](./" "![diagram](./images/" learning-path.md
+mmdc -i templates/learning-path.md -o ./images/learning-path.png -t neutral -b transparent -w 1000
+mmdc -i templates/learning-path.md -o ./images/learning-path.svg -t neutral -b transparent -w 1000
